eec2fb6ca739ea2cb7651a3198b7a4d6956bac5d
Welber Santos <welberos87@gmail.com>
2023-09-19 10:11:15 -0300

    Adicionar arquivos de projeto.

---
+++ ControleDeEmprestimoDeLivro/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ControleDeEmprestimoDeLivro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeEmprestimoDeLivro.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<EmprestimosModel> Emprestimos { get; set;}
+    }
+}
---
+++ ControleDeEmprestimoDeLivro/Models/EmprestimosModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeEmprestimoDeLivro.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id {  get; set; }
+        [Required(ErrorMessage = "Digite o nome do Recebedor!")]
+        public string Recebedor { get; set; }
+        [Required(ErrorMessage = "Digite o nome do Fornedor!")]
+        public string Fornecedor { get; set;}
+        [Required(ErrorMessage = "Digite o nome do Livro!")]
+        public string LivroEmprestado { get; set;}
+        public DateTime DataUltimaAtualizacao { get; set; } = DateTime.Now;
+        
+    }
+}
